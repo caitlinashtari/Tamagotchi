983fd3531fe42d627fa5abc6a7f35ede21916e3b
Caitlin Ashtari and Eric Bomblatus <eric@>
2016-11-29 11:47:42 -0800

    add initialize method and passing spec for name.

---
+++ lib/tamagotchi.rb
@@ -0,0 +1,16 @@
+class Tamagotchi
+  @@tamagotchi_levels = []
+
+  define_method(:initialize) do |name|
+    @name = name
+    @food_level = 10
+    @sleep_level = 10
+    @activity_level = 10
+  end
+
+  define_method(:name) do
+    @name
+  end
+
+
+end
